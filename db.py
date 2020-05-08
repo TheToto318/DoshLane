c8fe5235834841ddfcb266f12d3c2274556ba2e5
TheToto318 <36378675+TheToto318@users.noreply.github.com>
2020-05-08 17:28:44 +0200

    Add files via upload

--- db.py
+++ db.py
@@ -3,22 +3,45 @@ import sqlite3
 conn = sqlite3.connect("dashlane.db")
 c = conn.cursor()
 
-c.execute("CREATE TABLE IF NOT EXISTS Manager(User TEXT, Pass TEXT, Website TEXT)")
 
-
-def data_entry(User, Pass, Website):
-    "Algo Hashage et generation mdp"
-    c.execute("INSERT INTO Manager (User, Pass, Website) VALUES (?, ?, ?)", (User, Pass, Website))
+def create_user(Usermain, PassMain):
+    c.execute("CREATE TABLE User" + Usermain + "(User TEXT, PassMain TEXT, Pass TEXT, Website TEXT)")
+    c.execute("INSERT INTO User" + Usermain + "(PassMain) VALUES (?)", (PassMain,))
     conn.commit()
     c.close()
     conn.close()
+    succes = True
+
+
+def credential_check(User, Pass):
+    try:
+        c.execute("SELECT PassMain FROM User" + User + "")
+    except sqlite3.OperationalError:
+        print('Not exist')
+        succes = False
+    else:
+        dbpass = c.fetchall()
+        dbpass = (dbpass[0][0])
+        "Algo dehashage Pass"
+        if Pass == dbpass:
+            succes = True
+        else:
+                print('Error invalid password')
+                succes = False
 
 
 def read_user_info(User):
     "Algo dehasahge"
-    c.execute("SELECT * FROM Manager WHERE User=(?)", (User,))
+    c.execute("SELECT User, Pass, Website FROM User" + User + "")
     userdata = c.fetchall()
     print(userdata)
+
+
+def data_entry(Usermain, User, Pass, Website):
+    "Algo Hashage et generation mdp"
+    c.execute("INSERT INTO User" + Usermain + "(User, Pass, Website) VALUES (?, ?, ?)", (User, Pass, Website))
+    conn.commit()
+    c.close()
 
 
 def delete_all_user_info(User):
@@ -27,51 +50,48 @@ def delete_all_user_info(User):
     c.close()
 
 
-def delete_website(User, Pass, Website):
-    c.execute("SELECT Pass FROM Manager WHERE User=(?) AND Website=(?)", (User, Website))
+def delete_website(Usermain, Pass, Website):
+    c.execute("SELECT PassMain FROM User" + Usermain + "")
     dbpass = c.fetchall()
-    for row in dbpass:
-        dbpass = (row[0])
-    "Algo hashage Pass"
+    dbpass = (dbpass[0][0])
     if Pass == dbpass:
-        c.execute("DELETE from Manager WHERE User=(?) AND Website=(?)", (User, Website))
+        c.execute("DELETE from User" + Usermain + " WHERE Website=(?)", (Website,))
         conn.commit()
         c.close()
-        succes = 1
+        succes = True
     else:
         print('Error invalid password')
-        succes = 0
+        succes = False
 
 
-def update_password(User, Old, New, Website):
-    c.execute("SELECT Pass FROM Manager WHERE User=(?) AND Website=(?)", (User, Website))
-    dbold = c.fetchall()
-    for row in dbold:
-        dbold = (row[0])
+def update_password_website(Usermain, Passmain, New, Website):
+    c.execute("SELECT PassMain FROM User" + Usermain + "")
+    dbpass = c.fetchall()
+    dbpass = (dbpass[0][0])
     "Algo hashage pour old et new"
-    if Old == dbold:
-        c.execute("UPDATE Manager SET Pass=(?) WHERE Pass=(?) AND Website=(?)", (New, Old, Website))
+    print(dbpass)
+    if dbpass == Passmain:
+        c.execute("UPDATE User" + Usermain + " SET Pass=(?) WHERE Website=(?)", (New, Website,))
         conn.commit()
         c.close()
-        succes = 1
+        succes = True
     else:
         print('Error invalid password')
-        succes = 0
+        succes = False
 
 
-def update_user(Pass, newuser, Website):
-    c.execute("SELECT Pass FROM Manager WHERE Website=(?)", (Website,))
+def update_user(Usermain, Passmain, New, Website):
+    c.execute("SELECT PassMain FROM User" + Usermain + "")
     dbpass = c.fetchall()
-    for row in dbpass:
-        dbpass = (row[0])
+    dbpass = (dbpass[0][0])
     "Algo hashage pour Pass"
-    if Pass == dbpass:
-        c.execute("UPDATE Manager SET User=(?) WHERE Pass=(?) AND Website=(?)", (newuser, Pass, Website))
+    if dbpass == Passmain:
+        c.execute("UPDATE User" + Usermain + " SET User=(?) WHERE Website=(?)", (New, Website,))
         conn.commit()
         c.close()
-        succes = 1
+        succes = True
     else:
         print('Error invalid password')
-        succes = 0
+        succes = False
 
-
+read_user_info('Toto')
